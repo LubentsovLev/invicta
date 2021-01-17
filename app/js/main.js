7dab7fcf4571dbd0f19beadb12a3eb434182ce2d
lev_lubentsov <63476136+levheydude@users.noreply.github.com>
2021-01-17 18:01:49 +0300

    some

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+  $('.team__items').slick({
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  });
+  $('.head__slider').slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
+});
