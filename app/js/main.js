c27493e1fac2548b40780bd67cb70a0f0d209f33
lev_lubentsov <63476136+levheydude@users.noreply.github.com>
2021-02-07 20:10:43 +0300

    lotta

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,48 @@
 $(function () {
-  $('.team__items').slick({
-    infinite: false,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-  });
+  // if (document.documentElement.clientWidth < 768) {
+  //   $('.tariff__items').slick({
+  //     infinite: false,
+  //     slidesToShow: 1,
+  //     slidesToScroll: 1,
+  //   });
+  // }
   $('.head__slider').slick({
     infinite: false,
     slidesToShow: 1,
     slidesToScroll: 1,
   });
+  $('.team__items').slick({
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 720,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 650,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+});
+
+const burger = document.querySelector('.burger__burger');
+const nav = document.querySelector('.nav__js');
+const headerMob = document.querySelector('.header-mob');
+
+burger.addEventListener('click', function () {
+  this.classList.toggle('active');
+  nav.classList.toggle('nav_active');
+  headerMob.classList.toggle('active');
 });
